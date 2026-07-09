37ac7ef1f3ae18460bbad22a81300695ec8fcd63
Rohit Kr Thakur <r7256870923@gmail.com>
2026-07-09 17:24:59 +0530

    Modify update command to run with -y option

--- hello.sh
+++ hello.sh
@@ -3,4 +3,4 @@ echo "User : $(whoami)"
 echo "Hostname : $(hostname)"
 echo "Current Directory : $(pwd)"
 echo "Date : $(date)"
-echo "update : $(sudo apt update)
+echo "Update : $(sudo apt update -y)
